612fd458df9074b4bf24edf663c1201a1b506260
cshelor-conga2 <139390186+cshelor-conga2@users.noreply.github.com>
2025-06-07 09:29:42 -0400

    Changed radio options to picklist

--- Streamlit Migrate SF Files.py
+++ Streamlit Migrate SF Files.py
@@ -91,7 +91,11 @@ def create_zip(files):
 
 # -- New First Screen --
 st.subheader("Have you migrated all your Conga Template records into the target org?")
-migration_status = st.radio("", ("", "Yes", "No"), index=0 )
+migration_status = st.selectbox(
+    "",
+    options=["-- Select --", "Yes", "No"],
+    index=0
+)
 
 if migration_status == "Yes":
     # Show existing form
